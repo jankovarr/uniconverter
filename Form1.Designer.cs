41a7640008a7ce80afc03c3914eaab04e9cb993b
jankovarr <47503520+jankovarr@users.noreply.github.com>
2019-02-11 21:01:13 +0100

    Add files via upload
    base

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace UniConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chbHoo = new System.Windows.Forms.CheckBox();
+            this.chbHooAddsTest = new System.Windows.Forms.CheckBox();
+            this.chbBackToClip = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnAddstars = new System.Windows.Forms.Button();
+            this.btnTimestamp = new System.Windows.Forms.Button();
+            this.btnSigns = new System.Windows.Forms.Button();
+            this.chbCzechDelimiterOn = new System.Windows.Forms.CheckBox();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Location = new System.Drawing.Point(2, 21);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(420, 451);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            this.richTextBox1.Layout += new System.Windows.Forms.LayoutEventHandler(this.richTextBox1_Layout);
+            this.richTextBox1.MouseEnter += new System.EventHandler(this.richTextBox1_MouseEnter);
+            this.richTextBox1.MouseHover += new System.EventHandler(this.richTextBox1_MouseHover);
+            this.richTextBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.richTextBox1_PreviewKeyDown);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.richTextBox2.Location = new System.Drawing.Point(777, 21);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(411, 451);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            this.richTextBox2.MouseEnter += new System.EventHandler(this.richTextBox2_MouseEnter);
+            this.richTextBox2.MouseHover += new System.EventHandler(this.richTextBox2_MouseHover);
+            this.richTextBox2.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chbCzechDelimiterOn);
+            this.groupBox1.Controls.Add(this.chbHoo);
+            this.groupBox1.Controls.Add(this.chbHooAddsTest);
+            this.groupBox1.Controls.Add(this.chbBackToClip);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(1190, 63);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            this.groupBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 21);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 78);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "&Delete left side && Clipboard (if Auto CTRL+ C is ON)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chbHoo
+            // 
+            this.chbHoo.AutoSize = true;
+            this.chbHoo.Location = new System.Drawing.Point(614, 24);
+            this.chbHoo.Margin = new System.Windows.Forms.Padding(2);
+            this.chbHoo.Name = "chbHoo";
+            this.chbHoo.Size = new System.Drawing.Size(197, 24);
+            this.chbHoo.TabIndex = 2;
+            this.chbHoo.Text = "MouseHoover ON/OFF";
+            this.chbHoo.UseVisualStyleBackColor = true;
+            this.chbHoo.CheckStateChanged += new System.EventHandler(this.chbHoo_CheckStateChanged);
+            this.chbHoo.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // chbHooAddsTest
+            // 
+            this.chbHooAddsTest.AutoSize = true;
+            this.chbHooAddsTest.Location = new System.Drawing.Point(851, 23);
+            this.chbHooAddsTest.Margin = new System.Windows.Forms.Padding(2);
+            this.chbHooAddsTest.Name = "chbHooAddsTest";
+            this.chbHooAddsTest.Size = new System.Drawing.Size(203, 24);
+            this.chbHooAddsTest.TabIndex = 1;
+            this.chbHooAddsTest.Text = "MouseHoover adds text";
+            this.chbHooAddsTest.UseVisualStyleBackColor = true;
+            this.chbHooAddsTest.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // chbBackToClip
+            // 
+            this.chbBackToClip.AutoSize = true;
+            this.chbBackToClip.Checked = true;
+            this.chbBackToClip.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbBackToClip.Location = new System.Drawing.Point(358, 24);
+            this.chbBackToClip.Margin = new System.Windows.Forms.Padding(2);
+            this.chbBackToClip.Name = "chbBackToClip";
+            this.chbBackToClip.Size = new System.Drawing.Size(142, 24);
+            this.chbBackToClip.TabIndex = 0;
+            this.chbBackToClip.Text = "Auto CTRL + C";
+            this.chbBackToClip.UseVisualStyleBackColor = true;
+            this.chbBackToClip.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnPaste);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 211);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(1190, 474);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ctrl+V or Hoover to Convert";
+            this.groupBox2.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnAddstars);
+            this.groupBox3.Controls.Add(this.btnTimestamp);
+            this.groupBox3.Controls.Add(this.btnSigns);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(0, 63);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1190, 143);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Modes";
+            this.groupBox3.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // btnAddstars
+            // 
+            this.btnAddstars.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAddstars.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnAddstars.Location = new System.Drawing.Point(493, 22);
+            this.btnAddstars.Name = "btnAddstars";
+            this.btnAddstars.Size = new System.Drawing.Size(245, 118);
+            this.btnAddstars.TabIndex = 2;
+            this.btnAddstars.Text = "&Add  *...*  for SMODILOG";
+            this.btnAddstars.UseVisualStyleBackColor = false;
+            this.btnAddstars.Click += new System.EventHandler(this.btnAddstars_Click);
+            this.btnAddstars.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // btnTimestamp
+            // 
+            this.btnTimestamp.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnTimestamp.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnTimestamp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnTimestamp.Location = new System.Drawing.Point(248, 22);
+            this.btnTimestamp.Name = "btnTimestamp";
+            this.btnTimestamp.Size = new System.Drawing.Size(245, 118);
+            this.btnTimestamp.TabIndex = 1;
+            this.btnTimestamp.Text = "&Timestamp";
+            this.btnTimestamp.UseVisualStyleBackColor = false;
+            this.btnTimestamp.Click += new System.EventHandler(this.btnTimestamp_Click);
+            this.btnTimestamp.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // btnSigns
+            // 
+            this.btnSigns.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSigns.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSigns.Location = new System.Drawing.Point(3, 22);
+            this.btnSigns.Name = "btnSigns";
+            this.btnSigns.Size = new System.Drawing.Size(245, 118);
+            this.btnSigns.TabIndex = 0;
+            this.btnSigns.Text = "&Signs && Dots";
+            this.btnSigns.UseVisualStyleBackColor = false;
+            this.btnSigns.Click += new System.EventHandler(this.btnSigns_Click);
+            this.btnSigns.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // chbCzechDelimiterOn
+            // 
+            this.chbCzechDelimiterOn.AutoSize = true;
+            this.chbCzechDelimiterOn.Checked = true;
+            this.chbCzechDelimiterOn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbCzechDelimiterOn.Location = new System.Drawing.Point(73, 23);
+            this.chbCzechDelimiterOn.Margin = new System.Windows.Forms.Padding(2);
+            this.chbCzechDelimiterOn.Name = "chbCzechDelimiterOn";
+            this.chbCzechDelimiterOn.Size = new System.Drawing.Size(163, 24);
+            this.chbCzechDelimiterOn.TabIndex = 5;
+            this.chbCzechDelimiterOn.Text = "Czech delimiter \",\"";
+            this.chbCzechDelimiterOn.UseVisualStyleBackColor = true;
+            this.chbCzechDelimiterOn.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(426, 122);
+            this.btnPaste.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(232, 78);
+            this.btnPaste.TabIndex = 5;
+            this.btnPaste.Text = "Ctrl + &V";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            this.btnPaste.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 685);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "UniConverter v2";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Enter += new System.EventHandler(this.Form1_Enter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.btnAddstars_PreviewKeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chbBackToClip;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chbHooAddsTest;
+        private System.Windows.Forms.CheckBox chbHoo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnAddstars;
+        private System.Windows.Forms.Button btnTimestamp;
+        private System.Windows.Forms.Button btnSigns;
+        private System.Windows.Forms.CheckBox chbCzechDelimiterOn;
+        private System.Windows.Forms.Button btnPaste;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniConvert
+{
+    public partial class Form1 : Form
+    {
+        int lastChoice = 0;
+        // 1 - Sings,  2- Timestamp,  3- Add stars
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (lastChoice == 1)
+                btnSigns_Click(null, null);
+            else if (lastChoice == 2)
+                btnTimestamp_Click(null, null);
+            else if (lastChoice == 3)
+                btnAddstars_Click(null, null);
+            else
+            {
+                richTextBox1.Text = string.Empty;
+                richTextBox1.Text += "\nChoose Converter Mode first.\n";
+            }
+                
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            if (chbHoo.Checked)
+            {
+                int len = Clipboard.GetText().Length;
+              //  chbHooAddsTest.Enabled = true;
+                if (chbHooAddsTest.Checked)
+                    richTextBox1.Text += Clipboard.GetText() + (len == 0 ? "": "\n");
+                else
+                    richTextBox1.Text = Clipboard.GetText() + (len == 0 ? "" : "\n");
+            }
+            //else
+               // chbHooAddsTest.Enabled = false;
+           // chbHooAddsTest.Refresh();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox2_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox1_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox2_MouseHover(object sender, EventArgs e)
+        {
+            //Form1_MouseHover(null, null);
+        }
+
+        private void richTextBox1_MouseHover(object sender, EventArgs e)
+        {
+           Form1_MouseHover(null, null);
+        }
+
+        private void chbHoo_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (chbHoo.Checked)
+            {
+                
+                chbHooAddsTest.Enabled = true;
+              
+            }
+            else
+                chbHooAddsTest.Enabled = false;
+            chbHooAddsTest.Refresh();
+        }
+
+        private void Form1_Enter(object sender, EventArgs e)
+        {
+            //Form1_MouseHover(null, null);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                richTextBox1.Text = "";
+            }
+            else if (e.Control && e.KeyCode == Keys.V)
+                richTextBox1.Text = Clipboard.GetText();
+        }
+
+        private void groupBox2_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                richTextBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void btnSigns_Click(object sender, EventArgs e)
+        {
+            lastChoice = 1; setIndication();
+            if (sender != null) getTextFromClipboard();
+
+            string c = richTextBox1.Text;
+            string[] rows = c.Split('\n');
+            string result = string.Empty;
+            foreach (string line in rows)
+            {
+                string row = line;               
+                if (line.Length > 0 && line.Split('.').Length >= 0 && line.Split(',').Length >= 0 && line.Split('-').Length >= 0)
+                {
+                    // Obsahuje taby \t ? 
+                    // Ne:
+                    foreach (string tab in line.Split('\t'))
+                    {
+                        row = tab.Replace(".", "");
+                        if (row.Contains("-"))
+                        {
+                            row = "-" + row.Replace("-", "");
+
+                        }
+                        result += row + '\t';
+                    }
+                    string show = result.Substring(result.Length - 2, 1);
+                    if (show == "\t")
+                        result = result.Remove(result.Length - 2, 1); // remove last tab?
+
+                    result += '\n';
+                }
+                else if (line.Length > 0)
+                {
+
+                    result += row + '\n';
+                }
+            }
+
+            //Delimiter switch
+            if (!chbCzechDelimiterOn.Checked)
+                result = result.Replace(",", ".");
+            //else
+                //result = result.Replace(".", ",");
+
+            richTextBox2.Text = result;
+
+            bool isDiff = result != Clipboard.GetText() + "\n";
+            if (result != "" && isDiff && chbBackToClip.Checked) Clipboard.SetText(result);
+        }
+
+        private void getTextFromClipboard()
+        {   // to refactor...
+            if (chbHoo.Checked)
+                Form1_MouseHover(null, null);
+            else
+            {
+                int len = Clipboard.GetText().Length;
+                //  chbHooAddsTest.Enabled = true;
+                if (chbHooAddsTest.Checked)
+                    richTextBox1.Text += Clipboard.GetText() + (len == 0 ? "" : "\n");
+                else
+                    richTextBox1.Text = Clipboard.GetText() + (len == 0 ? "" : "\n");
+            }
+        }
+
+        private void btnTimestamp_Click(object sender, EventArgs e)
+        {
+            lastChoice = 2; setIndication();
+            if (sender != null) getTextFromClipboard(); //Otherwise it was already inserted.
+
+            string c = richTextBox1.Text;
+            string[] rows = c.Split('\n');
+            string result = string.Empty;
+            foreach (string line in rows)
+            {
+
+                string row = line;
+                // Timestamp case: LONG 20.160.921.201.424,0132910
+                if ((line.Split('.').Length == 4 && !line.Contains(',')) || (line.Split(',').Length >= 0))
+                {
+                    row = line.Replace(".", "");
+                    row = row.Replace(",", "");
+                    //string s = c.Replace("\\", "");
+                    //List<string> rows;
+                    //rows = ;
+
+                    //foreach (string row in clip)
+                    {
+                        //20.180.726.230.246
+                        if (row.Length == 14)
+                        { 
+
+                            int i = 4 + 2 + 2 + 2 + 2 + 2;
+                            int l = row.Length;
+                            result += row.TrimEnd().Substring(0, 4) + "." + row.Substring(4, 2) + "." + row.Substring(6, 2) + "\t";
+                            result += row.Substring(8, 2) + ":" + row.Substring(10, 2) + ":" + row.Substring(12, 2)
+                                 + row.Substring(14, l - i) + '\n';
+                            // + c.Substring()
+                            //result += row.
+                        }
+                        else if (row.Length >=14) //LONG 
+                        {
+                            int i = 4 + 2 + 2 + 2 + 2 + 2 + 2;
+                            int l = row.Length;
+                            result += row.TrimEnd().Substring(0, 4) + "." + row.Substring(4, 2) + "." + row.Substring(6, 2) + "\t";
+                            result += row.Substring(8, 2) + ":" + row.Substring(10, 2) + ":" + row.Substring(12, 2)
+                                 + "," + row.Substring(14, 2) + row.Substring(16, l - i) +  '\n';
+                        }
+
+                    }
+
+                    // copy to clip board ??
+                }
+                else if (line.Length > 0)
+                {
+
+                    result += row + '\n';
+                }
+
+
+
+            }
+
+            richTextBox2.Text = result;
+
+            bool isDiff = result != Clipboard.GetText() + "\n";
+            if (result != "" && isDiff && chbBackToClip.Checked) Clipboard.SetText(result);
+
+
+        }
+
+        private void btnAddstars_Click(object sender, EventArgs e)
+        {
+            lastChoice = 3; setIndication();
+            if (sender != null) getTextFromClipboard(); //Otherwise it was already inserted.
+
+            string c = richTextBox1.Text;
+            string[] rows = c.Split('\n');
+            string result = string.Empty;
+            foreach (string line in rows)
+            {
+                string row = line;
+                if (line.Length > 0)
+                {
+                    result += "*" + row + "*" + "\n";
+                }
+                else
+                    result += "\n";
+            }
+
+            richTextBox2.Text = result;
+
+            bool isDiff = result != Clipboard.GetText() + "\n";
+            if (result != "" && isDiff && chbBackToClip.Checked) Clipboard.SetText(result);
+        }
+
+        private void setIndication()
+        {
+            var OFF = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            var ON =  new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            btnAddstars.Font = lastChoice == 3 ? ON : OFF;
+            btnSigns.Font = lastChoice == 1 ? ON : OFF;
+            btnTimestamp.Font = lastChoice == 2 ? ON : OFF;
+        }
+
+        // First time Shown
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //btnAddstars_Click(null, null);
+            richTextBox1
+                .Focus();
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = Clipboard.GetText();
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+                btnPaste_Click(null, null);
+        }
+
+        private void richTextBox1_Layout(object sender, LayoutEventArgs e)
+        {
+            //richTextBox1
+              //  .Focus();
+        }
+
+        private void groupBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void groupBox3_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void btnAddstars_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+                btnPaste_Click(null, null);
+
+        }
+    }
+}
